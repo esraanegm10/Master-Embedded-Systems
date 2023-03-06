4d48b469ec5b270484fc33c7611d7684fb3e3614
esraa-negm <esraa.negm49@gmail.com>
2023-03-06 17:46:55 +0200

    EX1

---
+++ Unit_2_C_Programing/C_basics_Assignments/Assignment_1/EX1_Write_C_Program_to_Print_a_Sentence.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : EX1_Write_C_Program_to_Print_a_Sentence.c
+ Author      : esraa_negm
+ created on  : Mar 6, 2023
+ Description : Unit 2 C-Programing >> C-Basic/Assignment 1 >> EX1 C Program to Print a Sentence using Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+
+int main()
+{
+	printf("C Programming");
+
+	return 0;
+}
